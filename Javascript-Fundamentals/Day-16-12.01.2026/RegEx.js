1568469639e7343d0e9a64e595bef3d1bf3d71a4
amola09 <amol7anpat@gmail.com>
2026-01-13 18:43:39 +0530

    13.01.2026

---
+++ Javascript-Fundamentals/Day-16-12.01.2026/RegEx.js
@@ -0,0 +1,15 @@
+// RegEx: Regular Expression
+
+// pattern identification --> promt --> RegEx code
+
+// literal:
+let varOne = /hi/
+
+console.log("RegEx: ", varOne); // RegEx:  /hi/
+console.log(typeof varOne) // Object
+
+let varTwo = /Learn/
+console.log("RegEx:", varTwo)
+console.log(typeof varTwo)
+
+// class
