aa9b0ef4c419df1f2327c0c8ca6cc2a904078165
amola09 <amol7anpat@gmail.com>
2026-01-21 19:15:19 +0530

    21.01.2026

---
+++ Javascript-Fundamentals/Day-22-20.01.2026/ifElse.js
@@ -0,0 +1,51 @@
+// if-else:
+
+if (5 != 5) {
+    console.log("I am True");
+} else {
+    console.log("I am False");
+}
+
+
+// if else-if:
+
+if (5 != 5) {
+    console.log("I am True");
+} else if (5 > 3) {
+    console.log("I am else-if True");
+}
+
+// nested if-else:
+
+if (5 != 3) {
+    console.log("I am True");
+
+    if (true) {
+        console.log("I am nested True");
+    }
+
+} else {
+    console.log("I am False");
+}
+
+// if else
+    
+let marks = 45;
+
+if (marks >= 50) {
+  console.log("Pass");
+} else {
+  console.log("Fail");
+}
+
+
+// nested if else
+let score = 82;
+
+if (score >= 90) {
+  console.log("Grade A");
+} else if (score >= 70) {
+  console.log("Grade B");
+} else {
+  console.log("Grade C");
+}
