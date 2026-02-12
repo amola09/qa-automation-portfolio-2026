5dffb8600bd6a10ff3b97b823adc617cc1a61ae0
amola09 <amol7anpat@gmail.com>
2026-02-12 19:15:10 +0530

    12.02.2026

---
+++ PLAYWRIGHT/JS/revision2.js
@@ -0,0 +1,7 @@
+// string contact , string interpolation
+let firstName = "Amol"
+let lastName = "Raut"
+console.log("My firstName is "+firstName+" and lastName is "+lastName)
+console.log(`My firstName is ${firstName} and lastName is ${lastName}`)
+console.log(`${1+1}`)
+console.log(1+1)
