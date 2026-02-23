2d9a0909e6cb88eee5f6b9c2f08ea3fb20ff9aa8
amola09 <amol7anpat@gmail.com>
2026-02-23 16:31:26 +0530

    23-02-2026

---
+++ Javascript-Fundamentals/Day-40-13.02.2026/object destructure.js
@@ -0,0 +1,20 @@
+/* 
+Array: Position IMP
+let/var/const [address1, address2, ..., address_n] = [value1, value2, ..., value_n];
+
+
+syntax: Object --> keyName IMP
+let/var/const {keyName1, keyName2, ..., keyName_n} = {key:value,...}
+
+*/
+
+let obj = {
+    a: 10,
+    b: "Hi"
+}
+
+// retrive:
+// console.log(obj.a)
+
+// update:
+// obj.a = 20;
---
+++ Javascript-Fundamentals/Day-41-16.02.2026/spread&rest.js
@@ -0,0 +1,26 @@
+/* 
+   ...
+    ...objectName/variableName --> spread/rest 
+
+    spread --> separate the values
+    input: string, array, object
+    output: depends on the separate value
+
+    rest --> combine the values
+    input: any random entities
+    output: array/object
+
+
+ES6 feature
+
+*/
+
+let arr = [1, 2, 3, "JS"];
+// console.log(arr);
+// console.table(arr); // [1, 2, 3, "JS"];
+
+/* 
+  spread --> separate the values
+    input: string, array, object
+    output: depends on the separate value
+*/
